ccdba79411301627d9df853f0dfb4c53a25dced0
9jsartin <42806630+9jsartin@users.noreply.github.com>
2018-08-31 13:55:15 -0500

    Day before labor day weekend

--- vars.js
+++ vars.js
@@ -1,24 +1,24 @@
 let a = 10;
 let b = -25;
 //create two more variables c and d and give them whatever number you want.
-let c=
+let c= 9;
+let d=20;
 
 const total = a+b+c+d;
 console.log(total);
 
 
 
-
 //new section on string variables
 let convo = "hello ";
 //set name to your name, first and last.
-let name;
+let name= "Jaden Sartin";
 //add string convo and name together.
-const greeting =
+const greeting =convo+name;
 console.log(greeting);
 
 //make your own message similar to what is above.
-let convo2;
-let
-const
-console.log();
+let convo2= "Lamborgini";
+let convo3= "Ferrari";
+const Sports=convo2+convo3;
+console.log(Sports);
